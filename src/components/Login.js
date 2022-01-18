a9f4a5bed5298f1688649823d2e86c4c56490da2
Harold Bartolo <hbartolo@workapps.com.co>
2022-01-18 18:53:28 -0500

    API Gateway only once

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,83 +3,71 @@ import React from 'react';
 import { graphql } from '@apollo/react-hoc';
 import { gql, useQuery } from '@apollo/client';
 import { Card, InputGroup, FormControl, Button, DropdownButton, Dropdown, Form } from 'react-bootstrap';
+import loginService from '../services/loginService'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { getUserByNumber } from '../services/graphql'
 
 function Login ({ navigation }){
 
+  const [phone, setPhone] = useState("");
   const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordIncorret, setPasswordIncorret] = useState(false);
+  const [user, setUser] = useState(null);
+  const [invalidCredentials, setInvalidCredentials] = useState(false);
+  
 
-  const { loading, error, data } = useQuery(gql`
-  query {
-    usersByNumber(number : "${username}") {
-      Name
-      Number
-    } 
-  },
-  `);
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+      try{
+        const response = await loginService.Login({phone: phone, username: username});
+        if(response == null){
+          setInvalidCredentials(true);
+          return;
+        }
+        
+        setUser(response);
+        setInvalidCredentials(false);
+        console.log(response)
+      }
+      catch{
+        setInvalidCredentials(true);
+      }
 
 
-  const isVerifiedUser = () => {
-    return data?.usersByNumber?.Name === password;
+    // navigation.navigate('SignUp');
   }
 
-  const login =async () => {
-     console.log("username",username,"password", password, data)
-     const result = await isVerifiedUser();
-     
-    if (isVerifiedUser()) {
-      setPasswordIncorret(false);
-      window.location.replace('./');
-    } else {
-      setPasswordIncorret(true);
-    }
-  }
-  const goToSignUp = () => {
-    console.log(username);
-    console.log(password);
-    setPasswordIncorret(false);
-    navigation.navigate('SignUp');
-
-  }
+  
   return (
     <div className="container">
-       <Card className="text-center" style={{ width: '18rem' }}>
-     <Card.Body className="card-body">
-     <Card.Title className="tittle">Entra</Card.Title>
-     <Card.Subtitle className="mb-2 text-muted">Iris</Card.Subtitle>
-     <label>Ingresa tu numero de telefono</label>
-       <InputGroup className="mb-3">
-        
-        
-                        <FormControl
-                            placeholder="Numero de Telefono"
-                            onChange={e=> setUsername(e.target.value)}
-                            aria-label="Numero telefonico"
-                            aria-describedby="basic-addon1"
-                        />
-        </InputGroup>
-        <label>Ingresa tu nombre de usuario</label>
-        <InputGroup className="mb-3">
-                        <FormControl
-                            placeholder="Nombre De Usuario"
-                            onChange={e=> setPassword(e.target.value)}
-                            aria-label="Nombre De Usuario"
-                            aria-describedby="basic-addon1"
-                            value={password}
-                        />
-                        {(passwordIncorret && <p>
-        password or username incorrect
-      </p>)}
-        </InputGroup>
-        
-        <Button  onClick={login}>Entra</Button>
-     </Card.Body>
+      <Card className="text-center" style={{ width: '18rem' }}>
+        <Card.Title className="tittle">Entra</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Iris</Card.Subtitle>
+        <Card.Body className="card-body">
+        <Form onSubmit={handleLogin}>
+          <Form.Group className="mb-3" controlId="formBasicPhone">
+            <Form.Label>Ingresa tu número de teléfono</Form.Label>
+            <Form.Control type="phone" placeholder="Número de Teléfono" onChange={e=> setPhone(e.target.value)} />
+            <Form.Text className="text-muted">
+              {/*  We'll never share your email with anyone else. */}
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicUser">
+            <Form.Label>Ingresa tu nombre de usuario</Form.Label>
+            <Form.Control type="password" placeholder="Nombre de Usuario" onChange={e=> setUsername(e.target.value)}/>
+          </Form.Group>
+
+          {(invalidCredentials && <p> Número de teléfono o nombre de usuario incorrecto </p>)}
+
+          <Button variant="primary" type="submit">
+            Entra
+          </Button>
+        </Form>
+      </Card.Body>
      </Card>
     </div>
   );
 }
-
 
 export default Login;
