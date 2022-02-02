f37838647f838e22348b9b2b819dd19df56d4731
Harold Bartolo <habartolom@unal.edu.co>
2022-02-02 01:00:28 -0500

    Consuming API Gateway without JWT

--- src/services/authenticationService.js
+++ src/services/authenticationService.js
@@ -1,25 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { getUserByNumber, createUser } from './graphql';
 
 const url = 'http://host.docker.internal:7000'
 
 const login = async credentials => {
 
-    const requestBody = {
-        phone: credentials.phone,
-        name: credentials.username
-    }
+    // const requestBody = {
+    //     phone: credentials.phone,
+    //     name: credentials.username
+    // }
 
-    const response = await fetch(`${url}/account/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(requestBody)
-    });
+    // const response = await fetch(`${url}/account/login`, {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(requestBody)
+    // });
 
-    const result = await response.json();
-    const data = result.data;
-    return data;
+    // const result = await response.json();
+    // const data = result.data;
+    // return data;
+
+    const response = await getUserByNumber(credentials.phone);
+    if(credentials.username === response.Name)
+        return { token: null, user: response };
+    else
+        return null;
 }
 
 const register = async credentials => {
@@ -29,16 +36,23 @@ const register = async credentials => {
         name: credentials.username
     }
 
-    const response = await fetch(`${url}/account/register`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(requestBody)
-    });
+    // const response = await fetch(`${url}/account/register`, {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(requestBody)
+    // });
 
-    const result = await response.json();
-    const data = result.user;
-    return data;
+    // const result = await response.json();
+    // const data = result.user;
+    // return data;
+
+    const response = await createUser(requestBody);
+    if(response.ID != null)
+        return response;
+    else
+        return null;
 }
+
 export { login, register }
--- src/services/graphql.js
+++ src/services/graphql.js
@@ -1,11 +1,11 @@
 const url = 'http://host.docker.internal:7000/graphql';
 
-const buildRequest = async (query, token) => {
+const buildRequest = async (query/*, token*/) => {
     const response = await fetch(url, {
         method: 'POST', 
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + token,
+          'Content-Type': 'application/json'/*,
+          'Authorization': 'Bearer ' + token*/
         },
         body: JSON.stringify({ query: query })
     });
@@ -13,21 +13,22 @@ const buildRequest = async (query, token) => {
     return result;
 }
 
-const getUserByNumber = async (phone, token) => 
+const getUserByNumber = async (phone/*, token*/) => 
 {
     const query = `query {
         usersByNumber (number: "${phone}"){
+            ID
             Name
             Number
         }
     }`;
 
-    const response = await buildRequest(query, token);
+    const response = await buildRequest(query/*, token*/);
     const data = response.data?.usersByNumber;
     return data;
 };
 
-const getContactsByUserId = async (userId, token) => 
+const getContactsByUserId = async (userId/*, token*/) => 
 {
     const query = `query {
         contactsByUserId(userId: "${userId}") {
@@ -41,9 +42,27 @@ const getContactsByUserId = async (userId, token) =>
         }
     }`;
 
-    const response = await buildRequest(query, token);
+    const response = await buildRequest(query/*, token*/);
     const data = response.data?.contactsByUserId;
     return data;
 };
 
-export { getUserByNumber, getContactsByUserId }
+const createUser = async (credentials/*, token*/) => 
+{
+    const query = `mutation {
+        createUser(userAuth: {
+            Name: "${credentials.name}",
+            Number:"${credentials.phone}"
+        }){
+            ID
+            Name
+            Number
+        }
+    }`;
+
+    const response = await buildRequest(query/*, token*/);
+    const data = response.data?.createUser;
+    return data;
+};
+
+export { getUserByNumber, getContactsByUserId, createUser }
