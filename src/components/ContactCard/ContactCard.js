adf686efd1bf2b8f1697f059b85dac7be4892e08
Harold Bartolo <habartolom@unal.edu.co>
2022-02-07 01:25:10 -0500

    SSL Certificate - Change contact wallpaper

--- src/components/ContactCard/ContactCard.js
+++ src/components/ContactCard/ContactCard.js
@@ -14,14 +14,16 @@ function ContactCard ( { contact } ){
     
     return(
         <React.Fragment>
-            <div className="card m-3" style={{minWidth: "5rem"}} key={contact.contactID}>
+            <div className="card m-3" style={{minWidth: "5rem", width: "200px"}} key={contact.contactID}>
                 <div style={{minHeight: "200px", maxHeight: "200px"}} className="p-3">
                     <img style={{width: "150px"}} className="card-img-top" src={defaultContact} alt="Default contact"/>
                 </div>
                 <div className="card-body m-0">
                     <h5 className="card-title text-center">{contact.contactName}</h5>
                     <p className="card-text text-center">{contact.contactPhone}</p>
-                    <Button variant="primary" className="text-center" onClick={ getChat }>Escribir Mensaje</Button>
+                    <div className="d-flex flex-column justify-content-center align-items-center">
+                        <Button variant="primary" className="text-center" onClick={ getChat }>Escribir Mensaje</Button>
+                    </div>
                 </div>
             </div>
         </React.Fragment>
